92ea693bdc8fd4ffb3478d614f3fe9771f3e749d
nakhawa1108 <aknakhawa1108@gmail.com>
2023-07-07 23:52:43 +0530

    positional argument

---
+++ function.py
@@ -0,0 +1,14 @@
+def emp_details(id,name,city):
+    print("emp id=",id)
+    print("emp name=",name)
+    print('emp city=',city)
+emp_details(1,'adi','mumbai')
+emp_details('shri',2,'pune')
+
+def fruits(sweet,sour,seedless,seedful='papaya'):
+    print("sweet fruit is ",sweet)
+    print('sour fruit is ',sour)
+    print('seedless fruit is ',seedless)
+    print('seedful fruit is ',seedful)
+fruits('apple','orange','grapes')
+fruits('mango','lemon','pineapple','strawberry')
